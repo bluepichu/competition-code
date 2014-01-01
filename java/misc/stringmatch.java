905c198173acaab099a930ba0f05d2f7d8e1ea3b
BluePichu <matthew.damon.savage@gmail.com>
2014-01-01 12:07:23 -0500

    More indentation fixes.

--- java/misc/stringmatch.java
+++ java/misc/stringmatch.java
@@ -21,36 +21,36 @@
 // Returns:
 //   Integer[]      - the indices of the first characters for all instances of target in toMatch
 public static Integer[] stringMatchAll(String toMatch, String target){
-   int[] prefixes = new int[target.length()];
-   
-   int currentPos = 0;
-   for(int i = 1; i < target.length(); i++){
-      if(target.charAt(currentPos) == target.charAt(i)){
-         currentPos++;
-         prefixes[i] = currentPos;
-      } else {
-         currentPos = 0;
-      }
-   }
-   
-   ArrayList<Integer> matches = new ArrayList<Integer>();
-   
-   int currentMatch = 0;
-   for(int i = 0; i < toMatch.length(); i++){
-      if(toMatch.charAt(i) == target.charAt(currentMatch)){
-         currentMatch++;
-         if(currentMatch == target.length()){
-            matches.add(i+1-target.length());
-            currentMatch = prefixes[currentMatch-1];
-         }
-      } else {
-         currentMatch = prefixes[currentMatch];
-         i--;
-         continue;
-      }
-   }
-   
-   return matches.toArray(new Integer[0]);
+	int[] prefixes = new int[target.length()];
+	
+	int currentPos = 0;
+	for(int i = 1; i < target.length(); i++){
+		if(target.charAt(currentPos) == target.charAt(i)){
+			currentPos++;
+			prefixes[i] = currentPos;
+		} else {
+			currentPos = 0;
+		}
+	}
+	
+	ArrayList<Integer> matches = new ArrayList<Integer>();
+	
+	int currentMatch = 0;
+	for(int i = 0; i < toMatch.length(); i++){
+		if(toMatch.charAt(i) == target.charAt(currentMatch)){
+			currentMatch++;
+			if(currentMatch == target.length()){
+				matches.add(i+1-target.length());
+				currentMatch = prefixes[currentMatch-1];
+			}
+		} else {
+			currentMatch = prefixes[currentMatch];
+			i--;
+			continue;
+		}
+	}
+	
+	return matches.toArray(new Integer[0]);
 }
 
 // An implementation of the Knuth-Morris-Pratt algorithm
@@ -66,31 +66,31 @@ public static Integer[] stringMatchAll(String toMatch, String target){
 // Returns:
 //   int            - the index of the first character of the first instance of target in toMatch
 public static int stringMatchFirst(String toMatch, String target){
-   int[] prefixes = new int[target.length()];
-   
-   int currentPos = 0;
-   for(int i = 1; i < target.length(); i++){
-      if(target.charAt(currentPos) == target.charAt(i)){
-         currentPos++;
-         prefixes[i] = currentPos;
-      } else {
-         currentPos = 0;
-      }
-   }
-   
-   int currentMatch = 0;
-   for(int i = 0; i < toMatch.length(); i++){
-      if(toMatch.charAt(i) == target.charAt(currentMatch)){
-         currentMatch++;
-         if(currentMatch == target.length()){
-            return i+1-target.length();
-         }
-      } else {
-         currentMatch = prefixes[currentMatch];
-         i--;
-         continue;
-      }
-   }
-   
-   return matches.toArray(new Integer[0]);
+	int[] prefixes = new int[target.length()];
+	
+	int currentPos = 0;
+	for(int i = 1; i < target.length(); i++){
+		if(target.charAt(currentPos) == target.charAt(i)){
+			currentPos++;
+			prefixes[i] = currentPos;
+		} else {
+			currentPos = 0;
+		}
+	}
+	
+	int currentMatch = 0;
+	for(int i = 0; i < toMatch.length(); i++){
+		if(toMatch.charAt(i) == target.charAt(currentMatch)){
+			currentMatch++;
+			if(currentMatch == target.length()){
+				return i+1-target.length();
+			}
+		} else {
+			currentMatch = prefixes[currentMatch];
+			i--;
+			continue;
+		}
+	}
+	
+	return matches.toArray(new Integer[0]);
 }
